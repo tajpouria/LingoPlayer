5b2a484847e943ea0f305350ffee55160122bf6a
tajpouria <46822325+tajpouria@users.noreply.github.com>
2026-04-21 19:40:59 +0200

    feat: add icons metadata for improved application branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import { DarkModeProvider } from '@/src/DarkModeProvider';
 export const metadata: Metadata = {
   title: 'LingoPlayer',
   referrer: 'no-referrer',
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🎧</text></svg>',
+  },
 };
 
 export default function RootLayout({
